b9a4906b8a2176998346c2fcba3315c744c0b5b9
Arushi <aarushie2004@gmail.com>
2020-02-18 23:03:21 -0500

    Assignment 2

--- HW2/createIndexing.py
+++ HW2/createIndexing.py
@@ -1,65 +1,46 @@
 import argparse
 import os
 import io
-from nltk.stem.snowball import SnowballStemmer
-from nltk.stem.porter import *
-import re
 import pickle
-from itertools import islice
 
+from custom_util import my_tokenize, compress_line, encode_this, decode_this
+from custom_util import divide_chunks, read_stopwords, my_stemmer
 '''
 Running the file:
-with stem : python createIndexing.py --do_stem --outf output/custom_stem
-without stem : python createIndexing.py --outf output/custom_nostem
+with stem : python createIndexing.py --do_stem --outf output/stem_updated
+without stem : python createIndexing.py --outf output/unstemmed
+with compress : python createIndexing.py --do_stem --compress --outf output/stem_compressed/
 '''
 
-def save_data(data,vocab, fname):
-    print("Writing inverted index and vocab to pickle file %s.[vocab/index]" % fname)
-    pickle.dump(data, open("%s.index" % fname, "wb"))
-    pickle.dump(vocab, open("%s.vocab" % fname, "wb"))
 
-def my_stemmer(word):
-    '''
-        return stem of the word
-    '''
-    stemmer = PorterStemmer()
-    stem = stemmer.stem(word)
-    #stem = SnowballStemmer("english").stem(word)
-    return stem
+def write_to_file(partial_inverted_index, index, outf, compress):
+    catalog = {}
+    last_offset = 0
+    print("Writing partial_inverted_index_%d.txt" % index)
+    with io.open("%s/partial_inverted_index_%d.txt" % (outf, index), "wb") as f:
+        for term in partial_inverted_index:
+            ts = partial_inverted_index[term]["ts"]
+            fs = partial_inverted_index[term]["fs"]
+            ttf = fs["ttf"]
+            line = ["%d %s" % (k, ",".join(map(str, ts[k]))) for k in ts]
+            line = " ".join(line)
+            line = "%s %d %s\n" % (term, ttf, line) # term ttf docid1 pos1 docid2 pos1,pos2 docid3
+            line = compress_line(line, compress)
+            catalog[term] = [last_offset, len(line)]
+            last_offset += len(line)
+            f.write(line)
+    pickle.dump(catalog, open("%s/partial_catalog_%d.txt" % (outf, index), "wb"))
 
 
-def my_tokenize(text, do_stem, stopwords, vocab, docid):
-    '''
-        This function tokenizes the code and do stemming according to do_stem status(True/False)
-        U.S -> u . s .
-        (1, 20, 1), (2, 20, 2), (3, 20, 3), (4, 20, 4), (1, 20, 5), (2, 20, 6), (5, 20, 7)
-    '''
-    token_list = []
-    tokens = re.findall(r"([0-9\.0-9]+|[a-z]+|[\!\.,;:`-]+|[\w'\w]+)", text.lower())
-    position = 0
-    for token in tokens:
-        if token in stopwords:
-            continue
-        if do_stem:
-            token = my_stemmer(token)
-
-        if token not in vocab:
-            token_id = len(vocab) + 1
-            vocab[token] = token_id
-        else:
-            token_id = vocab[token]
-
-        position += 1
-        token_list.append([token_id, docid, position])
-    #print("Original Text : %s" % text)
-    #print(token_list)
-    return token_list, vocab
+def save_vocab(data, fname):
+    print("Writing vocab to pickle file %s/vocab.pickle" % fname)
+    pickle.dump(data, open("%s/vocab.pickle" % fname, "wb"))
 
 
 def read_data(path):
-    '''
-        reads all files in the directory in 'path'. doc_data is a dictionary with docid as key and text as val
-    '''
+    """
+    reads all files in the directory path. doc_data is a dictionary with current_docid as key and current_text as val
+    """
     doc_data = {}
     for fname in os.listdir(path):
         if not fname.startswith("ap"):
@@ -93,86 +74,91 @@ def read_data(path):
     print("len Doc data %d " % len(doc_data))
     return doc_data
 
-def read_stopwords(fname):
-    stopwords = set()
-    with io.open(fname, 'r', encoding='ISO-8859-1') as f:
-        for word in f:
-            stopwords.add(word.strip())
-    return stopwords
+def tokenize_build(text, do_stem, stopwords, vocab, docid):
+    """
+        This function tokenizes the code and do stemming according to do_stem status(True/False)
+        token_list -> (term1, 20, 1), (term2, 20, 2), (term3, 20, 3), (term4, 20, 4) [token, docid, term_position]
+    """
+    token_list = []
+    tokens = my_tokenize(text, do_stem, stopwords)
+    position = 0
+    for token in tokens:
+        if token not in vocab:
+            token_id = len(vocab) + 1
+            vocab[token] = token_id
+        else:
+           token_id = vocab[token]
 
-
-def divide_chunks(data, SIZE=1000):
-    it = iter(data)
-    for i in range(0, len(data), SIZE):
-        yield {k:data[k] for k in islice(it, SIZE)}
+        position += 1
+        token_list.append([token, docid, position])
+    return token_list, vocab
 
 
 def main(args):
+    # Creating output directory
+    if not os.path.exists(args.outf):
+        os.makedirs(args.outf)
+
+    # Reading doc data and dividing it into chunks of 1000
     doc_data = read_data(args.dir_path)
+    doc_data_chunks = divide_chunks(doc_data)
+
+    # Reading stopwords
     stopwords = read_stopwords(args.stopfile)
-    vocab = {}   #
-    docid_vocab = {}   #stored docnames as keys and docids(1,2,3) as values
-    inverted_index = {}
-    '''Divide the data in chunks of 1000'''
-    data = divide_chunks(doc_data)
-    for term in data:  #looping each term in the 1000 batch
-        for docname in doc_data:   #docname : AP890412-0196
-            text = doc_data[docname]
 
-            if docname not in docid_vocab:
-                docid_vocab[docname] = len(docid_vocab) + 1    #appending 1 to allot id to each docname
+    # token id and docIDn
+    vocab, docid_vocab = {}, {"ntoi": {}, "iton": {}}
+    sum_ttf = 0
+    doc_count = 0
+    doc_len = {}
 
-            tokens, vocab = my_tokenize(text, args.do_stem, stopwords, vocab, docid_vocab[docname])
-            #print("vocab size %d" % len(vocab))
+    for index, doc_chunk in enumerate(doc_data_chunks):  #looping each 1000 batch in total data
+        partial_inverted_index = {}
+        for docname in doc_chunk:   #docname : AP890412-0196
+            doc_count += 1
+            text = doc_chunk[docname]
 
-            for termid, docid, position in tokens:
-                if termid not in inverted_index:
-                    inverted_index[termid] = {"ts" : {docid : [1 , [position]]}}
-                    inverted_index[termid]["fs"] = {"df" : 1, "ttf" : 1}
+            # assigning ids to docnames and vice versa
+            if docname not in docid_vocab["ntoi"]:
+                new_idx = len(docid_vocab["ntoi"]) + 1 #appending 1 to allot unique id to each docname
+                docid_vocab["ntoi"][docname] = new_idx
+                docid_vocab["iton"][new_idx] = docname
+
+            tokens, vocab = tokenize_build(text, args.do_stem, stopwords, vocab, docid_vocab["ntoi"][docname])
+            doc_len[docname] = len(tokens)
+            for term, docid, position in tokens:
+                if term not in partial_inverted_index:
+                    partial_inverted_index[term] = {"ts": {docid: [position]}}
+                    partial_inverted_index[term]["fs"] = {"ttf": 1}
+                    sum_ttf += 1
                 else: # if term id is in inverted_index but docid is not there
-                    if docid not in inverted_index[termid]["ts"]:
-                        inverted_index[termid]["ts"][docid] = [1, [position]]
-                        inverted_index[termid]["fs"]["df"]+=1
-                        inverted_index[termid]["fs"]["ttf"] += 1
+                    if docid not in partial_inverted_index[term]["ts"]:
+                        partial_inverted_index[term]["ts"][docid] = [position]
+                        partial_inverted_index[term]["fs"]["ttf"] += 1
+                        sum_ttf += 1
                     else:
-                        inverted_index[termid]["ts"][docid][0]+=1
-                        inverted_index[termid]["ts"][docid][1].append(position)
-                        inverted_index[termid]["fs"]["ttf"] += 1
+                        partial_inverted_index[term]["ts"][docid].append(position)
+                        partial_inverted_index[term]["fs"]["ttf"] += 1
+                        sum_ttf += 1
 
-            '''
-                create indexing here
-                {term1:[docid1,count,[positions]}{
-                {
-                    'term1' : {
-                                 term_statistics : {
-                                                    docid1 : [
-                                                            tf , [pos1,pos22,pos34]
-                                                            ],
-                                                    docid2 : [
-                                                            tf , [pos10,pos221,pos341]
-                                                            ],
-                                                    }
-                                field_statistics:{
-                                                    DF : in how many docs does a term appear
-                                                    TTF : total no of the term in the corpus
-                                }
-                    'term2' : { docid3 : [tf,[pos8,pos220,pos134]], docid4 : [tf,[pos11,pos122,pos304]]}
-                }
-                DF: no of docs that contains the term -> len(inverted_index['term']
-                CF(TTF): 
-                list : [token_id, docid, position])
-            '''
+        write_to_file(partial_inverted_index, index, args.outf, args.compress)
     print("Vocab : %d" % len(vocab))
-    save_data(inverted_index, vocab, args.outf)
+    print("doc count %d:" % doc_count)
+    print("sum_ttf %d" % sum_ttf)
+    vocab = {"token_vocab": vocab, "doc_vocab": docid_vocab}
+    vocab["sum_ttf"] = sum_ttf
+    vocab["doc_count"] = doc_count
+    vocab["doc_len"] = doc_len
+    save_vocab(vocab, args.outf)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Arguments')
-    parser.add_argument("--index_name", type=str, default="hw1_dataset1", help="")
-    parser.add_argument("--dir_path", type=str, default="/Users/arushi/Documents/Spring2020/CS6200/Assignments/AP_DATA/ap89_collection", help="")
+    parser.add_argument("--dir_path", type=str, default="../data/AP_DATA/ap89_collection/", help="")
     parser.add_argument("--do_stem", action='store_true', help="")
-    parser.add_argument("--stopfile", type=str, default="stoplist.txt", help="")
+    parser.add_argument("--compress", action='store_true', help="")
+    parser.add_argument("--stopfile", type=str, default="data/stoplist.txt", help="")
     parser.add_argument("--outf", type=str, default="custom", help="")
     args = parser.parse_args()
-
+    print(args)
     main(args)
