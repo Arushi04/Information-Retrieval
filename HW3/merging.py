391a179f3f417e07b7d0928c48a02d4bea523cea
Arushi <aarushie2004@gmail.com>
2020-03-27 16:05:37 -0400

    removed comments

--- HW3/merging.py
+++ HW3/merging.py
@@ -17,9 +17,9 @@ def merge_inlinks(inlinks_list):
 
 
 def main(args):
-    es2 = Elasticsearch([{'host': 'localhost', 'port': 9200}])
-    #es2 = Elasticsearch("https://96aa4157ead74b5ca4926523b1d1994e.us-east-1.aws.found.io:9243",
-                              #  http_auth=('elastic', 'MrkfJ5hxIcCOzTMfOa1Nftzy'))
+    #es2 = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+    es2 = Elasticsearch("https://96aa4157ead74b5ca4926523b1d1994e.us-east-1.aws.found.io:9243",
+                                http_auth=('elastic', 'MrkfJ5hxIcCOzTMfOa1Nftzy'))
 
     #elasticsearch.helpers.reindex(es1, "church_data", args.out_index, query=None, target_client=None,
      #           chunk_size=500, scroll='5m', scan_kwargs={}, bulk_kwargs={})
@@ -44,20 +44,17 @@ def main(args):
 
         #Finding if the url is in the merged index
         result = es2.get(index=args.out_index, id=url, ignore=404)
-        #logging.info("Checking for result {}".format(result))
 
         if result['found'] is True:
             logging.info("inlinks from local  {}".format(len(set((frontier_map[url].inlinks)))))
             logging.info("inlinks retrieved {}".format(len(set(result['_source']['inlinks']))))
-            #retrieved_inlinks = result['_source']['inlinks']
             existing_inlinks = json.loads(j_inlinks)
             retrieved_inlinks = json.loads(result['_source']['inlinks'])
 
             #merging the inlinks from both local and merged set and updating the inlinks
             final_inlinkset = merge_inlinks([retrieved_inlinks, existing_inlinks])
             logging.info("length of final list {}".format(len(final_inlinkset)))
-            #es2.update(index=args.out_index, id=url, doc_type=args.doc_type, body={"doc": {"inlinks":json.dumps(final_inlinkset)}})
-            es2.update(index=args.out_index, id=url, body={"doc": {"inlinks": json.dumps(final_inlinkset)}})
+            es2.update(index=args.out_index, id=url, doc_type=args.doc_type, body={"doc": {"inlinks":json.dumps(final_inlinkset)}})
             filesupdated += 1
             logging.info("doc updated for url {}".format(url))
 
@@ -75,8 +72,7 @@ def main(args):
                 'inlinks': inlinks,
                 'outlinks': outlinks
             }
-            #es2.index(index=args.out_index, id=url, body=doc, doc_type=args.doc_type)
-            es2.index(index=args.out_index, id=url, body=doc)
+            es2.index(index=args.out_index, id=url, body=doc, doc_type=args.doc_type)
             filesadded += 1
             logging.info("doc added for url {}: ".format(url))
 
@@ -87,7 +83,7 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Arguments')
     parser.add_argument("--dir", type=str, default="./output/", help="")
-    parser.add_argument('-out', '--out_index', type=str, default="ir_hw03_prod", help="")
+    parser.add_argument('-out', '--out_index', type=str, default="ir_hw03_prod2", help="")
     parser.add_argument('-doc', '--doc_type', type=str, default="hw03_crawl_contents", help="")
     parser.add_argument("--ckp_no", type=int, default=40000, help="")
     args = parser.parse_args()
